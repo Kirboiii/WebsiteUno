df3d9c72c03c271183099f363c6b1a6929a2a764
Timothy Reaport <142744826+timothyreaport@users.noreply.github.com>
2024-02-27 22:31:42 +0000

    Tue, Feb 27, 2024, 2:31 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,10 @@
 loadText = document.querySelector('.loading-text')
 bg = document.querySelector('.bg')
-load = 0;
-loadText.style.opacity = 1
-bg.style.filter = "blur(30px)"
+load = 0
+int = setInterval(blurring, 100)
 function blurring() {
     load++
     loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(30px)"
 }
